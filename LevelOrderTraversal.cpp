96404528cbb9983180280c3390243eef5bee1f56
md_al_amin <mdalamin18053@gmail.com>
2023-11-24 22:26:19 +0600

    add not initial

---
+++ LevelOrderTraversal.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+vector<vector<int>> levelOrder(node *root)
+{
+    vector<vector<int>> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> level;
+        for (int i = 0; i < size; i++)
+        {
+            node *newNode = q.front();
+            q.pop();
+            if (newNode->left != NULL)
+            {
+                q.push(newNode->left);
+            }
+            if (newNode->right != NULL)
+            {
+                q.push(newNode->right);
+            }
+            level.push_back(newNode->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+int main()
+{
+    struct node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->right = new node(7);
+    root->right->right->left = new node(11);
+    vector<vector<int>> ans = levelOrder(root);
+    for (auto i : ans)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    return 0;
+}
---
+++ RBt.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void preorder(node *root) // Fix: Add a pointer type to the parameter
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(node *root) // Fix: Add a pointer type to the parameter
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(node *root) // Fix: Add a pointer type to the parameter
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    struct node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->right = new node(7);
+    root->right->right->left = new node(11);
+    preorder(root);
+    cout << "\n";
+    inorder(root);
+    cout << "\n";
+    postorder(root);
+    cout << "\n";
+    return 0; // Fix: Add a return statement
+}
