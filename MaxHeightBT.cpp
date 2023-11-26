ec4fedce92f54418f03483110911a169ff491f2d
md_al_amin <mdalamin18053@gmail.com>
2023-11-26 18:01:31 +0600

    3rd 2nd

--- MaxHeightBT.cpp
+++ MaxHeightBT.cpp
@@ -12,8 +12,7 @@ struct Node
         left = right = NULL;
     }
 };
-
-int maxHeight(Node *root)
+int maxHeight(Node *root) // using recursion of DFS
 {
     if (root == NULL)
     {
@@ -23,7 +22,42 @@ int maxHeight(Node *root)
     int r = maxHeight(root->right);
     return 1 + max(l, r);
 }
-
+int maxHeight1(Node *root) // using level order traversal of BFS
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    vector<int> lev(1000);
+    lev[root->data] = 1;
+    queue<Node *> q;
+    q.push(root);
+    int mx = 0;
+    while (!q.empty())
+    {
+        int size = q.size();
+        int l = q.front()->data;
+        mx = max(mx, lev[l]);
+        for (int i = 0; i < size; i++)
+        {
+            Node *newNode = q.front();
+            q.pop();
+            if (newNode->left != NULL)
+            {
+                Node *p = newNode->left;
+                q.push(p);
+                lev[p->data] = lev[l] + 1;
+            }
+            if (newNode->right != NULL)
+            {
+                Node *p = newNode->right;
+                q.push(p);
+                lev[p->data] = lev[l] + 1;
+            }
+        }
+    }
+    return mx;
+}
 int main()
 {
     Node *root = new Node(1);
@@ -35,6 +69,6 @@ int main()
     root->right->right->left = new Node(11);
 
     cout << maxHeight(root) << "\n"; // Fixed: Use << instead of <
-
+    cout << maxHeight1(root) << "\n";
     return 0; // Added: Return statement for main
 }
